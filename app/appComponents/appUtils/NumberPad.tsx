3f663ab04f7c1ab361f537cabc54e795a4795c88
Scriptscrypt <tirumaladasa@gmail.com>
2024-03-04 21:51:13 +0530

    Add: Numberpad styles & Functionality

--- app/appComponents/appUtils/NumberPad.tsx
+++ app/appComponents/appUtils/NumberPad.tsx
@@ -1,3 +1,4 @@
+import { colors4C, sizes4C } from "@/app/asthetics";
 import React, { useState } from "react";
 import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 
@@ -33,23 +34,75 @@ const NumberPad = () => {
     </TouchableOpacity>
   );
 
-  const numberButtons = Array.from({ length: 10 }, (_, index) =>
-    renderNumberButton(index)
+  const numberButtons = Array.from({ length: 9 }, (_, index) =>
+    renderNumberButton(index + 1)
   );
+
+  // Special Buttons code :
+  const handleSpecialButtonPress = (value: number) => {
+    setDisplayValue((prevValue) => {
+      const newValue = parseInt(prevValue, 10) + value;
+      return newValue <= 9999999999 ? newValue.toString() : prevValue;
+    });
+  };
+
+  const renderSpecialButton = (value: number, label: string) => (
+    <TouchableOpacity
+      key={label}
+      style={styles.helperButton}
+      onPress={() => handleSpecialButtonPress(value)}
+    >
+      <Text style={styles.helperButtonText}>{label}</Text>
+    </TouchableOpacity>
+  );
+
+  const specialButtons = [
+    { value: 50, label: "+50" },
+    { value: 150, label: "+150" },
+    { value: 500, label: "+500" },
+    { value: 1000, label: "+1000" },
+  ];
 
   return (
     <View style={styles.container}>
-      <View style={styles.displayContainer}>
-        <Text style={styles.displayText}>{displayValue}</Text>
+      <View style={styles.inputContainer}>
+        <View style={styles.displayContainer}>
+          <Text style={styles.displayText}>{displayValue}</Text>
+        </View>
+
+        {/* Special buttons */}
+        <View style={styles.buttonContainer}>
+          <View style={styles.rowContainer}>
+            {specialButtons.map((button) =>
+              renderSpecialButton(button.value, button.label)
+            )}
+          </View>
+        </View>
       </View>
-      <View style={styles.buttonContainer}>
-        {numberButtons}
-        <TouchableOpacity style={styles.button} onPress={handleClearPress}>
-          <Text style={styles.buttonText}>C</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={handleBackspacePress}>
-          <Text style={styles.buttonText}>⌫</Text>
-        </TouchableOpacity>
+
+      <View style={styles.numbersWrap}>
+        <View>
+          <View style={styles.buttonContainer}>
+            <View style={styles.rowContainer}>{numberButtons.slice(0, 3)}</View>
+            <View style={styles.rowContainer}>{numberButtons.slice(3, 6)}</View>
+            <View style={styles.rowContainer}>{numberButtons.slice(6, 9)}</View>
+            <View style={styles.rowContainer}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={handleClearPress}
+              >
+                <Text style={styles.buttonText}>Clear</Text>
+              </TouchableOpacity>
+              {renderNumberButton(0)}
+              <TouchableOpacity
+                style={styles.button}
+                onPress={handleBackspacePress}
+              >
+                <Text style={styles.buttonText}>⌫</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
       </View>
     </View>
   );
@@ -57,36 +110,77 @@ const NumberPad = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
+    flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
+    gap: sizes4C.small4C,
+    // backgroundColor: colors4C.green4C,
+  },
+  inputContainer: {
+    backgroundColor: colors4C.white4C,
+    padding: sizes4C.small4C,
+    borderRadius: sizes4C.small4C,
+  },
+  numbersWrap: {
+    padding: sizes4C.small4C,
+    backgroundColor: colors4C.white4C,
+    borderRadius: sizes4C.small4C,
   },
   displayContainer: {
-    backgroundColor: "#f0f0f0",
-    padding: 10,
-    margin: 10,
+    backgroundColor: colors4C.lightBlue4C,
+    borderWidth: 0.5,
+    borderColor: colors4C.lightGray4C,
+    padding: sizes4C.small4C,
+    margin: sizes4C.small4C,
     borderRadius: 8,
+    // width: "100%",
   },
   displayText: {
     fontSize: 24,
+    textAlign: "center",
+    fontWeight: "bold",
+    color: colors4C.blue4C,
   },
   buttonContainer: {
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  rowContainer: {
     flexDirection: "row",
-    flexWrap: "wrap",
     justifyContent: "center",
   },
   button: {
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#2196F3",
-    width: 64,
-    height: 64,
-    margin: 10,
+    backgroundColor: colors4C.light4C,
+    borderWidth: 0.4,
+    borderColor: colors4C.gray4C,
+    width: 112,
+    height: 40,
+    margin: 8,
     borderRadius: 4,
   },
   buttonText: {
-    fontSize: 24,
-    color: "white",
+    fontSize: 16,
+    color: colors4C.blue4C,
+  },
+  helperButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: colors4C.light4C,
+    borderWidth: 0.5,
+    borderColor: colors4C.gray4C,
+    width: 80,
+    height: 40,
+    margin: 8,
+    borderRadius: 4,
+  },
+
+  helperButtonText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: colors4C.gray4C,
   },
 });
 
