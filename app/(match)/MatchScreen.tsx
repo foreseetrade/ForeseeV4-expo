85560ae879c79ecccb65804519fd555c00cb8523
Scriptscrypt <tirumaladasa@gmail.com>
2024-03-06 18:23:06 +0530

    FKIT: CRACKK ANIMATION

--- app/(match)/MatchScreen.tsx
+++ app/(match)/MatchScreen.tsx
@@ -2,16 +2,29 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import MatchCard from "../appComponents/appCards/MatchCard";
 import YesNoSlider from "../appComponents/appCards/YesorNoSlider";
+import { sizes4C } from "../asthetics";
+import StatButton from "../appComponents/appButtons/StatButton";
+import MatchPredCard from "../appComponents/appCards/MatchPredCard";
 
 const MatchScreen = () => {
   return (
-    <View>
-      {/* <MatchCard /> */}
-      <YesNoSlider />
+    <View style={styles.container}>
+      <MatchCard />
+      <MatchPredCard winPercentage={60} />
+      <YesNoSlider teamA="DC" teamB="MI" teamAOdds={7} matchStadium="Chinnaswamy stadium" />
     </View>
   );
 };
 
 export default MatchScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "column",
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    justifyContent: "space-between",
+    padding: sizes4C.small4C,
+    gap: sizes4C.small4C,
+  },
+});
--- app/appComponents/appCards/YesorNoSlider.tsx
+++ app/appComponents/appCards/YesorNoSlider.tsx
@@ -3,21 +3,34 @@ import { View, Text, PanResponder, Animated, StyleSheet } from "react-native";
 
 // @ts-ignore
 import ballImg from "../../../assets/images/ball.png";
+import { colors4C, sizes4C } from "@/app/asthetics";
+import { LinearGradient } from "expo-linear-gradient";
+import { router } from "expo-router";
 
-const YesNoSlider = () => {
+const YesNoSlider = ({ teamA, teamB, teamAOdds, matchStadium }: any) => {
   const [sliderValue] = useState(new Animated.Value(0));
+  const [isRouting, setIsRouting] = useState(false); // New state
 
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
     onPanResponderMove: (event, gestureState) => {
       Animated.event([null, { dx: sliderValue }])(event, gestureState);
 
-      // Check the direction and log the appropriate message
-      if (gestureState.dx > 50) {
+      if (gestureState.dx > 98 && !isRouting) {
         console.log("Yes");
-      } else if (gestureState.dx < -50) {
+        setIsRouting(true); // Set the flag to prevent multiple calls
+        router.push("/(wallet)/TradeScreen");
+        return;
+      } else if (gestureState.dx < -98 && !isRouting) {
         console.log("No");
+        setIsRouting(true); // Set the flag to prevent multiple calls
+        router.push("/(wallet)/TradeScreen");
+        return;
       }
+    },
+    onPanResponderRelease: () => {
+      // Reset the flag when the user releases the pan responder
+      setIsRouting(false);
     },
   });
 
@@ -28,18 +41,50 @@ const YesNoSlider = () => {
   });
 
   return (
-    <View style={styles.container}>
-      <View style={styles.sliderContainer}>
-        <View style={styles.sliderTextWrapper}>
-          <Text style={styles.label}>₹7</Text>
-        </View>
-        <Animated.Image
-          source={ballImg}
-          style={[styles.slider, { transform: [{ translateX }] }]}
-          {...panResponder.panHandlers}
-        />
-        <View style={styles.sliderTextWrapper}>
-          <Text style={styles.label}>₹3</Text>
+    <View style={styles.wrapper}>
+      <Text style={styles.text}>
+        {" "}
+        Will {teamA} beat {teamB} at the {matchStadium} tonight?
+      </Text>
+      <View style={styles.container}>
+        <View style={styles.sliderContainer}>
+          <LinearGradient
+            colors={[
+              "#FFF",
+              "rgba(255, 255, 255, 0.25)",
+              "rgba(255, 255, 255, 0.00)",
+            ]}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 0 }}
+            style={{
+              ...styles.sliderTextWrapper,
+              borderBottomLeftRadius: 360,
+              borderTopLeftRadius: 360,
+            }}
+          >
+            <Text style={styles.label}>₹{teamAOdds}</Text>
+          </LinearGradient>
+          <Animated.Image
+            source={ballImg}
+            style={[styles.slider, { transform: [{ translateX }] }]}
+            {...panResponder.panHandlers}
+          />
+          <LinearGradient
+            colors={[
+              "rgba(255, 255, 255, 0.00)",
+              "rgba(255, 255, 255, 0.50)",
+              "#FFF",
+            ]}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 0 }}
+            style={{
+              ...styles.sliderTextWrapper,
+              borderBottomRightRadius: 360,
+              borderTopRightRadius: 360,
+            }}
+          >
+            <Text style={styles.label}>₹{10 - teamAOdds}</Text>
+          </LinearGradient>
         </View>
       </View>
     </View>
@@ -47,22 +92,40 @@ const YesNoSlider = () => {
 };
 
 const styles = StyleSheet.create({
+  wrapper: {
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-between",
+    gap: sizes4C.large4C,
+    backgroundColor: colors4C.purple4C,
+    // height: 120,
+    padding: sizes4C.medium4C,
+    borderRadius: sizes4C.small4C,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "600",
+    color: colors4C.white4C,
+  },
   container: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
     width: "100%",
+    // backgroundColor: "green",
   },
   sliderContainer: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
     width: "100%",
-    backgroundColor: "#ddd",
+    // backgroundColor: "#ddd",
     borderRadius: 360,
-    overflow: "hidden",
+    // overflow: "hidden",
+    height: 48,
   },
-  slider: { 
+  slider: {
+    zIndex: 1,
     resizeMode: "cover",
   },
   label: {
@@ -70,16 +133,18 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   sliderTextWrapper: {
-    backgroundColor: "#fff",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
+    // backgroundColor: colors4C.white4C,
+    // background: linear-gradient(90deg, rgba(255, 255, 255, 0.00) 0%, rgba(255, 255, 255, 0.50) 43.01%, #FFF 100.02%);
+    // shadowOffset: { width: 0, height: 2 },
+    // shadowOpacity: 0.25,
+    // shadowRadius: 3.84,
+    // elevation: 5,
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
     padding: 8,
-    width: "32%",
+    width: "40%",
+    height: "100%",
   },
 });
 
